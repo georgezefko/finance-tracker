0aa98683c88a05b2eb82e0983491a38731263269
georgezefko <georgioszefkilis@gmail.com>
2023-12-17 18:52:32 +0100

    feat(backend) first test

--- backend/src/controllers/feed.ts
+++ backend/src/controllers/feed.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+import { query } from '../database/db'
 
 export const getPosts = (_req: Request, res: Response, _next: NextFunction) => {
   res.status(200).json({
@@ -6,12 +7,18 @@ export const getPosts = (_req: Request, res: Response, _next: NextFunction) => {
   });
 };
 
-export const createPost = (req: Request, res: Response, _next: NextFunction) => {
+export const createPost = async (req: Request, res: Response, _next: NextFunction) => {
   const title = req.body.title;
   const content = req.body.content;
-  // Create post in db
-  res.status(201).json({
-    message: 'Post created successfully!',
-    post: { id: new Date().toISOString(), title: title, content: content }
-  });
+  
+  try {
+    const result = await query('INSERT INTO posts (title, content) VALUES ($1, $2) RETURNING *', [title, content]);
+    res.status(201).json({
+      message: 'Post created successfully!',
+      post: result.rows[0]
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Error saving post to database' });
+  }
 };
---
+++ backend/src/database/db.ts
@@ -0,0 +1,13 @@
+import { Pool, QueryResult } from 'pg';
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: 'tracker',
+  password: process.env.DB_PASS,
+  port: 5432,
+});
+
+export const query = (text: string, params: any[]): Promise<QueryResult> => {
+    return pool.query(text, params);
+  };
