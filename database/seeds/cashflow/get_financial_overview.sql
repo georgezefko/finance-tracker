a62f0581a2af7b31341abc9eb5f1e21784545b28
georgezefko <georgioszefkilis@gmail.com>
2024-02-01 23:44:46 +0100

    feat:new bar plot

--- database/seeds/cashflow/get_financial_overview.sql
+++ database/seeds/cashflow/get_financial_overview.sql
@@ -2,7 +2,7 @@ DROP FUNCTION IF EXISTS get_financial_metrics();
 
 CREATE OR REPLACE FUNCTION get_financial_metrics()
 RETURNS TABLE(
-    report_month TEXT,
+    report_yearly TEXT,
     total_income_value DECIMAL,
     expense_category VARCHAR,
     total_expense_value DECIMAL,
@@ -16,7 +16,7 @@ BEGIN
     WITH category_totals AS (
         SELECT 
             ec.category_name,
-            TO_CHAR(t.date, 'YYYY-MM') AS month,
+            TO_CHAR(t.date, 'YYYY') AS year,
             SUM(t.amount) AS total_amount,
             CASE 
                 WHEN ec.category_name = 'Income' THEN 'Income'
@@ -24,59 +24,59 @@ BEGIN
             END AS type
         FROM transactions t
         JOIN expense_categories ec ON t.category_id = ec.id
-        GROUP BY ec.category_name, TO_CHAR(t.date, 'YYYY-MM')
+        GROUP BY ec.category_name, TO_CHAR(t.date, 'YYYY')
     ),
     income_total AS (
         SELECT 
-            month,
+            year,
             SUM(total_amount) AS total_income
         FROM category_totals
         WHERE type = 'Income'
-        GROUP BY month
+        GROUP BY year
     ),
     expense_total AS (
         SELECT 
-            month,
+            year,
             category_name,
             SUM(total_amount) AS total_expense
         FROM category_totals
         WHERE type = 'Expense'
-        GROUP BY month, category_name
+        GROUP BY year, category_name
     ),
-    monthly_expenses AS (
+    yearly_expenses AS (
         SELECT
-            month,
-            SUM(total_expense) AS total_monthly_expenses
+            year,
+            SUM(total_expense) AS total_yearly_expenses
         FROM expense_total
-        GROUP BY month
+        GROUP BY year
     ),
     net_income AS (
         SELECT 
-            i.month,
-            (i.total_income - COALESCE(m.total_monthly_expenses, 0)) AS total_net_income
+            i.year,
+            (i.total_income - COALESCE(m.total_yearly_expenses, 0)) AS total_net_income
         FROM income_total i
-        JOIN monthly_expenses m ON i.month = m.month
+        JOIN yearly_expenses m ON i.year = m.year
     ),
       cumulative_net_income AS (
         SELECT 
-            month,
-            SUM(total_net_income) OVER (ORDER BY month) AS cumulative_income
+            year,
+            SUM(total_net_income) OVER (ORDER BY year) AS cumulative_income
         FROM net_income
     )
     SELECT 
-        i.month AS report_month,
+        i.year AS report_year,
         i.total_income AS total_income_value,
         e.category_name AS expense_category,
         (e.total_expense / NULLIF(i.total_income, 0)) * 100 AS total_expense_value,
-        (m.total_monthly_expenses / NULLIF(i.total_income, 0)) * 100 AS total_monthly_expenses,
+        (m.total_yearly_expenses / NULLIF(i.total_income, 0)) * 100 AS total_yearly_expenses,
         n.total_net_income  AS net_income_value,
         c.cumulative_income AS cumulative_net_income_value, 
         (n.total_net_income / NULLIF(i.total_income, 0)) * 100 AS savings_rate_value
     FROM income_total i
-    JOIN expense_total e ON i.month = e.month
-    JOIN monthly_expenses m ON i.month = m.month
-    JOIN net_income n ON i.month = n.month
-    JOIN cumulative_net_income c ON i.month = c.month
-    ORDER BY i.month, e.category_name;
+    JOIN expense_total e ON i.year = e.year
+    JOIN yearly_expenses m ON i.year = m.year
+    JOIN net_income n ON i.year = n.year
+    JOIN cumulative_net_income c ON i.year = c.year
+    ORDER BY i.year, e.category_name;
 END;
 $$ LANGUAGE plpgsql;
