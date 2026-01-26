6ec8581e8e626e5fa509d0ea0476e45048b82ebf
georgezefko <georgioszefkilis@gmail.com>
2026-01-26 16:32:14 +0100

    fix(backend): added prod variable

--- backend/src/db_conn/db.ts
+++ backend/src/db_conn/db.ts
@@ -2,12 +2,17 @@ import { Pool, QueryResult } from 'pg';
 import dotenv from 'dotenv';
 dotenv.config();
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const pool = new Pool({
   user: process.env.POSTGRES_USER,
   host: process.env.POSTGRES_HOST,
-  database: 'tracker',
+  database: process.env.POSTGRES_DB,
   password: process.env.POSTGRES_PASSWORD,
   port: 5432,
+  ssl: isProd
+    ? { rejectUnauthorized: false } // Neon, managed DBs
+    : false, 
 });
 
 export const query = (text: string, params?: any[]): Promise<QueryResult> => {
