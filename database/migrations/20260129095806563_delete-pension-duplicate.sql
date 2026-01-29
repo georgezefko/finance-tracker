880cc0d5066118b56f781066f26775319399f003
georgezefko <georgioszefkilis@gmail.com>
2026-01-29 12:30:57 +0100

    fix(migrations): corrected drop duplicates

--- database/migrations/20260129095806563_delete-pension-duplicate.sql
+++ database/migrations/20260129095806563_delete-pension-duplicate.sql
@@ -2,14 +2,15 @@
 
 BEGIN;
 
--- 1) Delete the duplicate Pension type (adjust id if needed)
 DELETE FROM networth_types
 WHERE type_name = 'Pension'
   AND id = 4;
 
--- 2) Make sure each (type_name, category_id) pair is unique
 ALTER TABLE networth_types
-ADD CONSTRAINT IF NOT EXISTS unique_type_per_category
+DROP CONSTRAINT IF EXISTS unique_type_per_category;
+
+ALTER TABLE networth_types
+ADD CONSTRAINT unique_type_per_category
 UNIQUE (type_name, category_id);
 
 COMMIT;
